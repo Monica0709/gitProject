0b7d558d22d4bb5fa2e6485fc218a2be6eb2c6f4
monica <monica.surendar@gmail.com>
2023-05-05 17:23:22 +0530

    Git

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -1,7 +1,7 @@
 package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// this is demo for GitHub
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
